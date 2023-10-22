d941e7560a486b5e14d74bc23b6229d3592a7189
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-22 23:26:15 +0400

    Task3

---
+++ Tyuiu.ShabukovDV.Sprint3.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabukovDV.Sprint3.Task3.V11.Lib;
+
+namespace Tyuiu.ShabukovDV.Sprint3.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "sqwreq vqwqq q";
+            char replaceable = 'q';
+            char replacement = '7';
+            string res = ds.ReplaceCharOnNum(str, replaceable, replacement);
+            string wait = "s7wre7 v7w77 7";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShabukovDV.Sprint3.Task3.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShabukovDV.Sprint3.Task3.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShabukovDV.Sprint3.Task3.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18074e34-85da-49bd-9b10-a747dd5e62db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
