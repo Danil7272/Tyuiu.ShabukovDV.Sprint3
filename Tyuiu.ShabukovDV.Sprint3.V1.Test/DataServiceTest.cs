2a4d9ca0a7e26abbe5a69f245141fe05eb41a56c
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-22 23:01:01 +0400

    Add project files.

---
+++ Tyuiu.ShabukovDV.Sprint3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabukovDV.Sprint3.V1.Lib;
+
+namespace Tyuiu.ShabukovDV.Sprint3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service1 = new DataService();
+            Assert.AreEqual(1935, service1.GetSumSeries(4, 1, 15));
+        }
+    }
+}
