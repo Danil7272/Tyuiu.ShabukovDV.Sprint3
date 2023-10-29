fae2d270201f45eaf0443943613b3d90f46f8037
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-29 21:57:58 +0400

    Task5

--- Tyuiu.ShabukovDV.Sprint3.Task5.V16/Program.cs
+++ Tyuiu.ShabukovDV.Sprint3.Task5.V16/Program.cs
@@ -12,6 +12,41 @@ namespace Tyuiu.ShabukovDV.Sprint3.Task5.V16
     {
         static void Main(string[] args)
         {
+            Console.Title = "Спринт #3 | Выполнил: Шабуков Д. В. | СМАРТб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Шабуков Д. В. | СМАРТб-23-1                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* y = sum i = 1 to 3 sum k = 1 to 10 cos(k) + x ^ 2 при x = 2.            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
         }
     }
 }
