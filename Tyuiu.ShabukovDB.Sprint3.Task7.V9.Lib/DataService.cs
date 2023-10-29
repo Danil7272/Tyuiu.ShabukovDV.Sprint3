9aeddb3f9a904ae3389639f759af0ef6396f2626
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-29 22:19:04 +0400

    Task7

--- Tyuiu.ShabukovDB.Sprint3.Task7.V9.Lib/DataService.cs
+++ Tyuiu.ShabukovDB.Sprint3.Task7.V9.Lib/DataService.cs
@@ -7,7 +7,23 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.ShabukovDB.Sprint3.Task7.V9.Lib
 {
-    public class DataService
+    public class DataService : ISprint3Task7V9
     {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((2 * x - 3) / (Math.Cos(x) - 2 * x) + 5 * x - Math.Sin(x), 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+
+        }
     }
 }
--- Tyuiu.ShabukovDB.Sprint3.Task7.V9/Program.cs
+++ Tyuiu.ShabukovDB.Sprint3.Task7.V9/Program.cs
@@ -11,6 +11,51 @@ namespace Tyuiu.ShabukovDB.Sprint3.Task7.V9
     {
         static void Main(string[] args)
         {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Шабуков Д. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Шабуков Д. В. | СМАРТб-23-1                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции.           *");
+            Console.WriteLine("* Произвести табулирование на заданном диапазоне.Значения занести в массив*");
+            Console.WriteLine("*                                                                         *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = dataService.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+
+            for (int i = 0; i < res.Length; i++, startValue++)
+            {
+                Console.WriteLine("|{0,5:d}     | {1,5:f2}    |", startValue, res[i]);
+            }
+
+            Console.WriteLine("+----------+-----------+");
+
+            Console.ReadKey();
+
         }
     }
 }
